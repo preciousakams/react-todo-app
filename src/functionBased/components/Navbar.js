421d04c24ae7cc33b9442a4de0dfac01756a2104
Precious Akams <preciousakams@yahoo.com>
2024-01-20 18:01:26 +0000

    testing

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
 
 const NavBar = () => (
   <nav className="navBar">
@@ -9,12 +9,12 @@ const NavBar = () => (
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
-      <li className="aboutNav">
+      {/* <li className="aboutNav">
         <DropdownButton id="dropdown-basic-button" title="About">
           <Dropdown.Item href="/author">Author</Dropdown.Item>
           <Dropdown.Item href="/app">About App</Dropdown.Item>
         </DropdownButton>
-      </li>
+      </li> */}
     </ul>
     <Outlet />
   </nav>
